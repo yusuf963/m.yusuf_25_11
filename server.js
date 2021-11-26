4713afa6d04a22c455451e61d92cc629caaa9b29
yusuf963 <mohammad963yusuf@gmail.com>
2021-11-26 12:57:17 +0000

    addin config file to production

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const connectionDB = require('./config/db');
+const path = require('path');
 const productRoute = require('./view/garments');
 // require('dotenv/config');
 const app = express();
 
 app.use(cors());
 app.use('/', productRoute);
+//serve static asset in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use('/', (req, res) => {
   res.status(200).send('server is running');
 });
