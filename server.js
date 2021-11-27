16c4bf28927f247d1c5b1ac9a089d516e9dcee86
yusuf963 <mohammad963yusuf@gmail.com>
2021-11-27 13:39:49 +0000

    clear git cache

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const cors = require('cors');
 const connectionDB = require('./config/db');
 const path = require('path');
 const productRoute = require('./view/garments');
-require('dotenv').config({ path: 'variables.env' });
-// require('dotenv/config');
 const app = express();
 
 app.use(cors());
 app.use('/', productRoute);
 //serve static asset in production
 if (process.env.NODE_ENV === 'production') {
+  console.log('env',process.env.NODE_ENV)
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   //dev enviroments
+  console.log('env',process.env.NODE_ENV)
   app.use('/', (req, res) => {
     res.status(200).send('server is running');
   });
